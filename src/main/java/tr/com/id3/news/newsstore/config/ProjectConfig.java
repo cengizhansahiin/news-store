a4335915df89d0ef50499a6db98f87a7b9044ceb
Cengizhan Şahin <cengizhan.sahin@id3.com.tr>
2022-07-29 20:26:49 +0300

    NWSP-8 Reading data from ActiveMQ Queue is completed.

---
+++ src/main/java/tr/com/id3/news/newsstore/config/ProjectConfig.java
@@ -0,0 +1,73 @@
+package tr.com.id3.news.newsstore.config;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jms.DefaultJmsListenerContainerFactoryConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
+import org.springframework.jms.config.JmsListenerContainerFactory;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+import javax.jms.ConnectionFactory;
+
+@ComponentScan("tr.com.id3.news.newsstore")
+@Configuration
+@EnableJms
+@EnableAutoConfiguration
+@PropertySource("application.properties")
+public class ProjectConfig {
+    @Value("${spring.activemq.broker-url}")
+    String BROKER_URL;
+    @Value("${spring.activemq.user}")
+    String BROKER_USERNAME;
+    @Value("${spring.activemq.password}")
+    String BROKER_PASSWORD;
+
+    @Bean
+    public ActiveMQConnectionFactory connectionFactory(){
+        ActiveMQConnectionFactory connectionFactory = new  ActiveMQConnectionFactory();
+        connectionFactory.setTrustAllPackages(true);
+        connectionFactory.setBrokerURL(BROKER_URL);
+        connectionFactory.setPassword(BROKER_USERNAME);
+        connectionFactory.setUserName(BROKER_PASSWORD);
+        return connectionFactory;
+    }
+
+    @Bean
+    public MessageConverter messageConverter() {
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        converter.setTargetType(MessageType.TEXT);
+        converter.setObjectMapper(new ObjectMapper());
+        return converter;
+    }
+
+    @Bean
+    public JmsListenerContainerFactory jmsListenerContainerFactory(ConnectionFactory connectionFactory,
+                                                                   DefaultJmsListenerContainerFactoryConfigurer configurer) {
+        DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
+        configurer.configure(factory, connectionFactory);
+        return factory;
+    }
+
+    @Bean
+    public JmsTemplate jmsTemplate() {
+        JmsTemplate jmsTemplate = new JmsTemplate();
+        jmsTemplate.setConnectionFactory(connectionFactory());
+        jmsTemplate.setDeliveryPersistent(true);
+        jmsTemplate.setMessageConverter(messageConverter());
+        return jmsTemplate;
+    }
+
+
+}
+
---
+++ src/main/java/tr/com/id3/news/newsstore/model/Article.java
@@ -0,0 +1,17 @@
+package tr.com.id3.news.newsstore.model;
+
+import lombok.Data;
+
+
+@Data
+public class Article {
+
+    private String author;
+    private String title;
+    private String description;
+    private String url;
+    private String urlToImage;
+    private String publishedAt;
+    private String content;
+
+}

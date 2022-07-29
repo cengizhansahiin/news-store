d92725963aea412573e9936fb6a407bfc6fc2205
Cengizhan Şahin <cengizhan.sahin@id3.com.tr>
2022-07-29 16:33:15 +0300

    NWSP-9 Initial Commit Setup

---
+++ src/main/java/tr/com/id3/news/newsstore/NewsStoreApplication.java
@@ -0,0 +1,13 @@
+package tr.com.id3.news.newsstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tr/com/id3/news/newsstore/NewsStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package tr.com.id3.news.newsstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
